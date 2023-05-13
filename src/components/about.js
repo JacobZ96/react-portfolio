0588ab0bf4b781b6729477dbfea0e419d5ed29bc
WalleyemanZ <jacobzea96@gmail.com>
2023-05-13 15:02:49 -0600

    updated app.js and added content to about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,1 +1,28 @@
-import React, { useState } from "react";    
+import React, { useState } from "react";
+const aboutMe = () => {
+  return (
+    <div>
+      <h1 id="about-me">About Jacob</h1>
+      <div id="about-me-section">
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/77647073?v=4"
+            alt="Jacob Zea"
+            id="about-me-img"
+          />
+        </div>
+        <div id="about-me-bio">
+          <p>
+            I'm originally from Sioux Falls, SD, I moved to Denver, CO in 2020
+            with my wife. I have a strong background in customer service and
+            inventory management. I graduated college in 2019 with degrees in
+            Conservation Biology and Sustainability from the University of South
+            Dakota.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default aboutMe;
