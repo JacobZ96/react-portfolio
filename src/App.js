8070bcdd11b5255a4e5b8927791873e626e6946b
WalleyemanZ <jacobzea96@gmail.com>
2023-05-13 15:38:00 -0600

    added images folder, added content to footer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
-// import NavBar from './components/NavBar';
-// import Header from './components/Header';
+// import NavBar from './components/nav';
+// import Header from './components/header';
 import AboutMe from './components/about';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
+// import Projects from './components/projects';
+// import Contact from './components/contact';
+// import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
-      {/* <Header /> */}
-      <AboutMe />
-      {/* <Projects /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      {/* <NavBar/> */}
+      {/* <Header/> */}
+      <AboutMe/>
+      {/* <Projects/> */}
+      {/* <Contact/> */}
+      {/* <Footer/> */}
     </div>
   );
 }
