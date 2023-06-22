2fc057c4b697a7194d90a81af97351dc2df4705b
jacob zea <jacobzea96@gmail.com>
2023-06-21 22:01:33 -0600

    updated download resume link, and about me paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+          <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+          I'm a full stack web developer with a background in retail inventory
+          management and customer service looking to make a career change. I
+          recently earned a certificate in full stack web development from the
+          University of Denver, where I developed skills in JavaScript, CSS,
+          React.js, and responsive web design. I'm excited to leverage my skills
+          as part of a fast-paced, quality-driven team to build better
+          experiences on the web.
         </p>
       </div>
     </section>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://docs.google.com/document/d/1GzXouGerkcLkiCgORnwDRX5fQsdH7Sjn3M34_czDGNc/edit?usp=sharing" target="_blank" rel="noreferrer">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
